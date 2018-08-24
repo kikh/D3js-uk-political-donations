a89981f3fd939a985de54655f7b41c0894162cea
kikh <p13mout@ionio.gr>
2018-08-24 20:26:45 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -429,7 +429,7 @@ $(document).ready(function() {
 
 });
 
-$("p, h1, h2, h3, strong").lettering('words');
-$('p, h1, h2, h3, strong').mouseover(function(event) {
+$("a, p, h1, h2, h3, h4, h5, strong").lettering('words');
+$('a, p, h1, h2, h3, h4, h5 strong').mouseover(function(event) {
     var word=event.target.innerHTML;
 });
